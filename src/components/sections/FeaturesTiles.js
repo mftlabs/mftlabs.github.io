f7a5fc7670f1e8a8a0e01a2e005b893b3e281bbc
aram0112 <48899851+aram0112@users.noreply.github.com>
2022-06-14 21:18:41 -0500

    initial landing page

---
+++ src/components/sections/FeaturesTiles.js
@@ -0,0 +1,217 @@
+import React from "react";
+import classNames from "classnames";
+import { SectionTilesProps } from "../../utils/SectionProps";
+import SectionHeader from "./partials/SectionHeader";
+import Image from "../elements/Image";
+
+const propTypes = {
+  ...SectionTilesProps.types,
+};
+
+const defaultProps = {
+  ...SectionTilesProps.defaults,
+};
+const FeaturesTiles = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  pushLeft,
+  ...props
+}) => {
+  const outerClasses = classNames(
+    "features-tiles section",
+    topOuterDivider && "has-top-divider",
+    bottomOuterDivider && "has-bottom-divider",
+    hasBgColor && "has-bg-color",
+    invertColor && "invert-color",
+    className
+  );
+
+  const innerClasses = classNames(
+    "features-tiles-inner section-inner pt-0",
+    topDivider && "has-top-divider",
+    bottomDivider && "has-bottom-divider"
+  );
+
+  const tilesClasses = classNames(
+    "tiles-wrap center-content",
+    pushLeft && "push-left"
+  );
+
+  const sectionHeader = {
+    title: "Everything you need in one place",
+    paragraph:
+      "AMPS has all the tools you need to handle the backend infrastructure of your business or application. Easily interface with a variety of services and protocols, create API services to perform custom logic, manage and deploy Python-based applications that can plug-in to AMPS topic-based messaging, and more.",
+  };
+
+  return (
+    <section {...props} className={outerClasses}>
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={tilesClasses}>
+            <div
+              className="tiles-item reveal-from-bottom"
+              data-reveal-delay="200"
+            >
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require("./../../assets/images/feature-tile-icon-02.svg")}
+                      alt="Features tile icon 02"
+                      width={64}
+                      height={64}
+                    />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">Extensibility</h4>
+                  <p className="m-0 text-sm">
+                    Leverage the simplicity of Python to efficiently and easily
+                    extend the capabilities of AMPS and manage your python-based
+                    applications.{" "}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="tiles-item reveal-from-bottom">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require("./../../assets/images/feature-tile-icon-01.svg")}
+                      alt="Features tile icon 01"
+                      width={64}
+                      height={64}
+                    />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">Agility</h4>
+                  <p className="m-0 text-sm">
+                    Leverage AMPS' horizontally scaling topic-based subscribers
+                    to efficiently and dynamically meet your messaging needs.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div
+              className="tiles-item reveal-from-bottom"
+              data-reveal-delay="400"
+            >
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require("./../../assets/images/feature-tile-icon-03.svg")}
+                      alt="Features tile icon 03"
+                      width={64}
+                      height={64}
+                    />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">Flexibility</h4>
+                  <p className="m-0 text-sm">
+                    AMPS' topic-based, metadata-driven workflows allow for
+                    incredible flexibility in how messages are routed,
+                    processed, and delivered.{" "}
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div
+              className="tiles-item reveal-from-bottom"
+              data-reveal-delay="200"
+            >
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require("./../../assets/images/feature-tile-icon-05.svg")}
+                      alt="Features tile icon 05"
+                      width={64}
+                      height={64}
+                    />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">Reliability</h4>
+                  <p className="m-0 text-sm">
+                    Built in Elixir on the robust Erlang Runtime, AMPS is
+                    resilient and self-healing, ensuring that your AMPS-managed
+                    services are always available. Easily administer your
+                    services via the UI, and leave the infrastructure to AMPS.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require("./../../assets/images/feature-tile-icon-04.svg")}
+                      alt="Features tile icon 04"
+                      width={64}
+                      height={64}
+                    />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">Compatibility</h4>
+                  <p className="m-0 text-sm">
+                    Easily interface with a number of existing messaging
+                    protocols and services with built-in actions and services
+                    for S3, SQS, HTTP, SFTP, SharePoint, Kafka, and more!
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div
+              className="tiles-item reveal-from-bottom"
+              data-reveal-delay="400"
+            >
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <Image
+                      src={require("./../../assets/images/feature-tile-icon-06.svg")}
+                      alt="Features tile icon 06"
+                      width={64}
+                      height={64}
+                    />
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">Visibility</h4>
+                  <p className="m-0 text-sm">
+                    AMPS gives you complete visibility on the system and the
+                    messages that it processes. Easily search through message
+                    events to retry failed actions or confirm successful message
+                    delivery. Additionally, use AMPS' convenient logs to
+                    diagnose system issues, retry failed actions, and monitor
+                    user activity.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+FeaturesTiles.propTypes = propTypes;
+FeaturesTiles.defaultProps = defaultProps;
+
+export default FeaturesTiles;
